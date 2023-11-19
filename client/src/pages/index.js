54b9069b20d434761ed93d9ff46d1f9dfdaa40e4
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2023-11-19 15:29:14 +0000

    update

--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from "../pages/About/About"
-export { default as Reservation } from "../pages/Reservation/Reservation"
-export { default as Chefs } from "../pages/Chefs/Chefs"
-export { default as Footer } from "../pages/Footer/Footer"
-export { default as Menu } from "../pages/Menu/Menu"
-export { default as Navbar } from "../pages/Navbar/Navbar"
--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const Reservation = require('./models/Reservation'); // Make sure the path is correct
+const Reservation = require('./models/Reservation');
 
 const app = express();
 
 // Connect to MongoDB
-const mongoURI = "mongodb+srv://developmentsimsek:noirpass@cluster-1.ouc5riu.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
